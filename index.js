5ff518ba775a8c00cbe686d954ac72735d120387
Zajith Corro <zajithcorrov@hotmail.com>
2021-10-16 23:12:29 -0500

    :sparkles: Formating and rendering results
    Function to format the given list to calculate the values and function to render them in the screen

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const listArray = [12, 6, 7, 3, 15, 10, 18, 5];
+const form = document.querySelector('#form');
 
 function average(listNumbers) {
   const sumOfTerms = listNumbers.reduce((prev, current) => prev + current, 0);
@@ -20,8 +21,37 @@ function variance(average, listNumbers) {
   return totalSumOfTerms / totalOfTerms;
 }
 
-const averageCalculated = average(listArray);
-const varianceCalculated = variance(averageCalculated, listArray);
+function stringToNumberList(valueInput) {
+  const formatedString = valueInput.replace(/[\[\]]|\s/g, '');
+  const listOfString = formatedString.split(',');
 
-console.log(`Average: ${averageCalculated}`);
-console.log(`Population variance: ${varianceCalculated}`);
+  return listOfString.map((el) => parseInt(el));
+}
+
+function renderResults(average, variance, listOfNums) {
+  const resultsContainer = document.querySelector('.results');
+
+  if (resultsContainer.classList.contains('d-none'))
+    resultsContainer.classList.remove('d-none');
+
+  resultsContainer.innerHTML = `<h2 class="results-title">Results</h2>
+  <p><strong>List of numbers: </strong>${listOfNums}</p>
+  <p><strong>Average: </strong>${average.toFixed(4)}</p>
+  <p><strong>Population variance: </strong>${variance.toFixed(4)}</p>`;
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const input = document.querySelector('#input-values');
+  const valueInput = input.value;
+
+  if (!valueInput) return;
+
+  const listOfNums = stringToNumberList(valueInput);
+
+  const averageCalculated = average(listOfNums);
+  const varianceCalculated = variance(averageCalculated, listOfNums);
+
+  renderResults(averageCalculated, varianceCalculated, valueInput);
+});
